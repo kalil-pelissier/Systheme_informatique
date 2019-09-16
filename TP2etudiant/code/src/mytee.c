05f297ac1ed993f950ca67ee5681eab5f58e00ed
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-09-17 00:12:33 +0200

    problème toujours présent

--- TP2etudiant/code/src/mytee.c
+++ TP2etudiant/code/src/mytee.c
@@ -1,20 +1,25 @@
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
+
+	
 
 void mytee(char* path) {
 	char buf; 
-	int rd;
-	int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	do
-	{
+	int rd, fd;
+	fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if(fd<0) {
+		printf("%s%d\n", "problème pour ouvrir le fichier", fd);
+		exit(1);
+	}
+	do{
 		rd = read(0, &buf, sizeof(char));
 		write(fd, &buf, sizeof(char));
-
 	} while (rd == sizeof(char));
-	
 }
+
+
 
 int main(int argc, char argv[]) {
 
