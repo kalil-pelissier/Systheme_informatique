7ef8b0e597ea1db8d41e9e392b5f003e75961f31
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-09-23 16:16:16 +0200

    ajout d'un nouveau fichier

--- TP2etudiant/code/src/mytee.c
+++ TP2etudiant/code/src/mytee.c
@@ -2,15 +2,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 	
 
-void mytee(char* path) {
+void mytee(char path) {
 	char buf; 
 	int rd, fd;
-	fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd = open(&path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if(fd<0) {
-		printf("%s%d\n", "problème pour ouvrir le fichier", fd);
+		printf("%d\n", fd);
 		exit(1);
 	}
 	do{
