7701656f2331af43c7285c81e409000b87ebef5f
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-11-04 16:48:37 +0100

    dernnier malloc en cours

--- TP3/code/src/memalloc.2.c
+++ TP3/code/src/memalloc.2.c
@@ -5,46 +5,71 @@
 typedef struct bloc_entete
 {
  size_t taille; //taille du bloc utilisateur
- void* next; //next block free
- void* previous;//previous block free
- unsigned short libre : 1 ; //drapeau de 1 bit qui indique si le bloc est libre ou utilise: 1 libre, 0 utilise.
+ struct bloc_entete* next; //next block free
+ struct bloc_entete* previous;//previous bloc
 } bloc_entete ;
 
-void* pastFree;
-void* firstFree;
+struct bloc_entete* firstFree;
+void* teteFree;
 int init = 0;
 
 void bloc_info2(void* ptr){
     bloc_info1(ptr);
 }
 
+struct bloc_entete* getLibre(size_t t){
+	bloctete* i = firstFree; 
+	while(i->next =/ null && i->taille < t){
+		i = i->next;
+	}
+	if(i->taille >= t){
+		return i;
+	}else{
+		return null;
+	}
+}
+
 //todo
 void* myalloc2(size_t t)
 {
-	void* current = firstFree;
-	if(init == 0) {
-		return myalloc0(t);
+	struct bloc_entete* recycle = getLibre(t);
+	{
+		
+	};
+	if(init == 0 || recycle == null){
+		int taille = ALIGN(t);
+	  	void* ptr = sbrk(taille);
+	  
+	  	//initialisation de mon entet
+	  	bloc_entete* entete = (bloc_entete*)ptr;
+	  	entete->taille = taille;
+	  	entete->next = null;
+	  	entete->previous = null;
+
+	 	ptr = ptr+ENTETE_SIZE;
+	  	return ptr;
 	}
-	else if()
+	else if(recycle =/ null)
+	{
+		return (void*)recycle+ENTETE_SIZE;	
+	}
 };
 
 
 //todo
 void myfree2(void* ptr)
 {
-	if(init == 0) {
-		firstFree = ptr-ENTETE_SIZE;
-		currentFree = firstFree;
+	if(init == 0){
+		firstFree = ptr-ENENTETE_SIZE;  
+		teteFree = firstFree;
 		init = 1;
-		bloc_entete* free = (bloc_entete*)currentFree;
-		t->libre = 1;
-	}else {
-		void* temp = ptr-ENTETE_SIZE;
-		bloc_entete* t = (bloc_entete*)temp;
-		bloc_entete* f = (bloc_entete*)pastFree; //je récupère l'entete du derrnier free
-		t->libre = 1;
-		f->next = temp;
-		pastFree = temp;
+		//on verra plus tard si on utilise la variable libre ou pas
+	}else{
+		void* current = ptr-ENENTETE_SIZE;
+		bloc_entete* bloc = (bloc_entete*)current;
+		bloc_entete* bloctete = (bloc_entete*)teteFree;
+		bloc->previous = bloctete;
+		bloctete->next = bloc;
+		teteFree = current; 
 	}
-  
 }
